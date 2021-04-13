aa13ccc0f6e8202127ed216441bc66b60354ddae
yot2imyb <liujun1239518@163.com>
2021-04-13 09:21:41 +0800

    Initial Commit

---
+++ 4-6/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  4-6
+//
+//  Created by Mac OS on 16-4-6.
+//  Copyright (c) 2016年 liaomengshi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
